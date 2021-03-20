ee292b1eefbc2f9bb32f6d0d41ae16faaa4a3a10
Tatiana Nosova <tatiananosova746@gmail.com>
2021-03-20 09:19:13 -0500

    Lesson2 Homework

---
+++ src/lesson2/Main.java
@@ -0,0 +1,118 @@
+package lesson2;
+
+import java.util.Random;
+import java.util.function.*;
+
+public class Main {
+    public static void main(String[] args) {
+//        int[] arr = new int[10];
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = i;
+//        }
+//
+//        System.out.println(Arrays.toString(arr));
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] % 2 != 0) {
+//                arr[i] *=3;
+//            }
+//        }
+
+//        System.out.println(Arrays.toString(arr));
+
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.print(arr[i]+" ");
+//        }
+
+//        for (String s : strarr) {
+//            System.out.print(s);
+//        }
+
+
+//        String[] strarr = {"asd", "zxc", "qwe"};
+//
+//        System.out.println(Arrays.toString(strarr));
+//
+////        for (int i = 0; i < strarr.length; i++) {
+////            strarr[i] += "0";
+////        }
+//
+//        for (String s : strarr) {
+//            s += "0";
+//        }
+//
+//        System.out.println(Arrays.toString(strarr));
+
+
+//        List<Integer> list = new ArrayList<>(Arrays.asList(2, 2, 2, 3, 56, 2, 4, 3, 2));
+
+//        System.out.println(list);
+////        list.removeIf(i -> i == 2);
+////        list.removeAll(Arrays.asList(2));
+////        list.replaceAll(i -> i*i);
+////        list.remove((Integer) 2);
+//
+//        System.out.println(list);
+
+
+
+//        MyArrayList<Integer> mal = new MyArrayList<>();
+//        mal.add(5);
+//        mal.add(8);
+//        mal.add(2);
+//        System.out.println(mal);
+//
+//        mal.add(1, 99);
+//        System.out.println(mal);
+//
+////        System.out.println(mal.remove(2));
+//        System.out.println(mal.remove((Integer) 99));
+//        System.out.println(mal);
+
+
+//        MySortedArrayList<Integer> msal = new MySortedArrayList<>();
+//        msal.add(5);
+//        msal.add(1);
+//        msal.add(8);
+//        msal.add(2);
+//        msal.add(3);
+//        System.out.println(msal);
+//        msal.add(4);
+//        System.out.println(msal);
+//        System.out.println(msal.binaryIndexOf(1));
+
+
+        Random r = new Random();
+        MyArrayList<Integer> mal = new MyArrayList<>(100000);
+        for (int i = 0; i < 100000; i++) {
+            mal.add(r.nextInt(100));
+        }
+        System.out.println("size " + mal.size());
+
+        runWithTimeCount(MyArrayList::shuffle, mal, "Shuffle");
+        runWithTimeCount(MyArrayList::insertionSort, mal, "Insertion sort");
+        runWithTimeCount(MyArrayList::shuffle, mal, "Shuffle");
+        runWithTimeCount(MyArrayList::selectionSort, mal, "Selection sort");
+        runWithTimeCount(MyArrayList::shuffle, mal, "Shuffle");
+        runWithTimeCount(MyArrayList::bubbleSort, mal, "Bubble sort");
+
+        long time = System.currentTimeMillis();
+//        mal.selectionSort();
+// Sort time 10536 mls
+//        mal.insertionSort();
+// Sort time 9426 mls
+//        mal.bubbleSort();
+// Sort time 37507 mls
+        System.out.println(System.currentTimeMillis() - time);
+
+  //      System.out.println(mal);
+
+    }
+
+    public static void runWithTimeCount(Consumer<MyArrayList<Integer>> consumer, MyArrayList<Integer> mal, String msg) {
+        long time = System.currentTimeMillis();
+        consumer.accept(mal);
+        System.out.println(msg + " run time is: " + (System.currentTimeMillis() - time) + " ms");
+    }
+}
---
+++ src/lesson2/MyArrayList.java
@@ -0,0 +1,192 @@
+package lesson2;
+
+import java.util.Random;
+
+public class MyArrayList<T extends Comparable<T>> {
+    private T[] list;
+    private int size;
+    private int capacity;
+    private final int DEFAULT_CAPACITY = 1000000;
+
+    public MyArrayList(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("capacity <=0 " + capacity);
+        }
+        this.capacity = capacity;
+        list = (T[]) new Comparable[capacity];
+    }
+
+    public MyArrayList() {
+        this.capacity = DEFAULT_CAPACITY;
+        list = (T[]) new Comparable[capacity];
+    }
+
+    public void add(T item) {
+        // доделать проверку на превышение лоад фактора 0.75
+        checkLoadFactor();
+        list[size] = item;
+        size++;
+    }
+
+    private void checkLoadFactor() {
+        if ((double)size/capacity > 0.75) {
+            System.out.println("Load factor reached. Increasing capacity");
+            capacity = (int) (capacity + (capacity * 0.25));
+            T[] newList = (T[]) new Comparable[capacity];
+            System.arraycopy(list, 0, newList, 0, size);
+            this.list = newList;
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new RuntimeException("incorrect index");
+        }
+    }
+
+    public void add(int index, T item) {
+        // доделать проверку на превышение лоад фактора 0.75
+        checkLoadFactor();
+        // доделать проверку на допустимость индекса
+        checkIndex(index);
+        for (int i = size; i > index; i--) {
+            list[i] = list[i - 1];
+        }
+        list[index] = item;
+        size++;
+    }
+
+    public final T remove(int index) {
+        // доделать проверку на допустимость индекса
+        checkIndex(index);
+        T temp = list[index];
+        for (int i = index; i < size; i++) {
+            list[i] = list[i + 1];
+        }
+        size--;
+        list[size] = null;
+        return temp;
+    }
+
+    public boolean remove(T item) {
+        int i = indexOf(item);
+        if (i == -1) {
+            return false;
+        }
+        remove(i);
+        return true;
+    }
+
+    public int indexOf(T item) {
+        for (int i = 0; i < size; i++) {
+            if (list[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public T get(int index) {
+        // доделать проверку на допустимость индекса
+        checkIndex(index);
+        return list[index];
+    }
+
+    public void set(int index, T item) {
+        checkIndex(index);
+        // доделать проверку на допустимость индекса
+
+        list[index] = item;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[ ");
+        for (int i = 0; i < size; i++) {
+            sb.append(list[i]).append(", ");
+        }
+        if (size > 0) {
+            sb.setLength(sb.length() - 2);
+        }
+
+        sb.append(" ]");
+        return sb.toString();
+    }
+
+
+    private boolean less(T item1, T item2) {
+        return item1.compareTo(item2) < 0;
+    }
+
+    private void swap(int index1, int index2) {
+        T temp = list[index1];
+        list[index1] = list[index2];
+        list[index2] = temp;
+    }
+
+    public void selectionSort() {
+        int iMin;
+        for (int i = 0; i < size - 1; i++) {
+            iMin = i;
+            for (int j = i + 1; j < size; j++) {
+                if (less(list[j], list[iMin])) {
+                    iMin = j;
+                }
+            }
+            swap(i, iMin);
+        }
+    }
+
+    public void insertionSort() {
+        T key;
+        for (int i = 1; i < size; i++) {
+            int j = i;
+            key = list[i];
+            while (j > 0 && less(key, list[j - 1])) {
+                list[j] = list[j - 1];
+                j--;
+            }
+            list[j] = key;
+        }
+    }
+
+    public void bubbleSort() {
+        for (int i = size - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (less(list[j + 1], list[j])) {
+                    swap(j + 1, j);
+                }
+            }
+        }
+    }
+
+    public void bubbleSortO() {
+        boolean isSwap;
+        for (int i = size - 1; i > 0; i--) {
+            isSwap = false;
+            for (int j = 0; j < i; j++) {
+                if (less(list[j + 1], list[j])) {
+                    swap(j + 1, j);
+                    isSwap = true;
+                }
+            }
+            if( !isSwap){
+                break;
+            }
+        }
+    }
+
+    public void shuffle() {
+        Random rnd = new Random();
+        for (int i = size - 1; i > 0; i--) {
+            int index = rnd.nextInt(i);
+            T a = list[index];
+            list[index] = list[i];
+            list[i] = a;
+        }
+    }
+}
