89c219d61b124a5e27260e198051d2263d65cfb4
Tatiana Nosova <tatiananosova746@gmail.com>
2021-04-01 16:32:40 -0500

    Lesson5 added folder Homework

---
+++ src/lesson5/homework/Item.java
@@ -0,0 +1,12 @@
+package lesson5.homework;
+
+public class Item {
+
+    int price;
+    int weight;
+
+    public Item(int price, int weight) {
+        this.price = price;
+        this.weight = weight;
+    }
+}
