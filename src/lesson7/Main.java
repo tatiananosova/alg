046eaead17dcb22d170bd09d99f07e06eb3c047e
Tatiana Nosova <tatiananosova746@gmail.com>
2021-04-03 22:18:30 -0500

    lesson7Homework

--- src/lesson7/Main.java
+++ src/lesson7/Main.java
@@ -4,22 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Graph graph = new Graph(10);
         graph.addEdge(1,2);
-        graph.addEdge(0,4);
-        graph.addEdge(1,4);
-        graph.addEdge(3,4);
+        graph.addEdge(0,3);
+        graph.addEdge(2,4);
+        graph.addEdge(3,5);
+        graph.addEdge(4,8);
+        graph.addEdge(5,1);
+        graph.addEdge(6,7);
         graph.addEdge(7,8);
-
-        graph.addEdge(0,1);
+        graph.addEdge(8,9);
+        graph.addEdge(9,0);
 
 //        System.out.println(graph.getAdjList(4));
 
-//        DepthFirstPath dfp = new DepthFirstPath(graph, 0);
-//        System.out.println(dfp.hasPathTo(2));
-//        System.out.println(dfp.pathTo(2));
+        DepthFirstPath dfp = new DepthFirstPath(graph, 1);
+        System.out.println(dfp.hasPathTo(3));
+        System.out.println(dfp.pathTo(3));
 
-        BreadthFirstPath bfp  = new BreadthFirstPath(graph, 0);
-        System.out.println(bfp.hasPathTo(2));
-        System.out.println(bfp.pathTo(2));
+        BreadthFirstPath bfp  = new BreadthFirstPath(graph, 1);
+        System.out.println(bfp.hasPathTo(3));
+        System.out.println(bfp.pathTo(3));
 
 
 
